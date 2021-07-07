486238eabcb2a9a930cfd25c8a8070f56657a9af
pache_pot <heedoo0303@naver.com>
2021-07-08 08:08:47 +0900

    # https://www.acmicpc.net/problem/2805

---
+++ BOJ/2805.java
@@ -0,0 +1,132 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+    static long M;
+    static int[] H;
+
+    public static void main (String[] args) throws Exception {
+
+        solve();
+
+        bw.write(sb.toString());
+        bw.flush();
+
+    }
+
+    static void solve () throws Exception {
+
+        N = readInt(); M = readInt();
+        H = new int[N];
+        for (int i=0; i<N; i++) H[i] = readInt();
+
+        binarySearch(0, 1000000000);
+
+    }
+
+    static void binarySearch (int left, int right) {
+
+        int mid=(left+right)/2;
+
+        if (left>right) {
+            sb.append(mid);
+            return;
+        }
+
+        else if (tree(mid)>=M) binarySearch(mid+1,right);
+
+        else if (tree(mid)<M) binarySearch(left,mid-1);
+
+
+    }
+
+    static long tree (int cut) {
+
+        long sum = 0;
+        for (int i=0; i<N; i++) if (H[i]-cut>0) sum+= H[i]-cut;
+        return sum;
+
+    }
+
+    static int readInt() {
+        return in.nextInt();
+    }
+
+    static long readLong() {
+        return in.nextLong();
+    }
+
+    static double readDouble() {
+        return in.nextDouble();
+    }
+
+    static String readLine() {
+        return in.nextLine();
+    }
+
+    static String read() {
+        return in.next();
+    }
+
+    static void exit (String s) {
+
+        System.out.println(s);
+        System.exit(0);
+
+    }
+
+}
+
+class FastReader {
+
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+
+}
