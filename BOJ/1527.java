590adbcc9227a937e06a8619ca11050f138c648f
pache_pot <heedoo0303@naver.com>
2021-07-25 15:18:14 +0900

    # https://www.acmicpc.net/problem/1527

---
+++ BOJ/1527.java
@@ -0,0 +1,130 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int A, B ,count;
+
+    static Stack<Integer> stack = new Stack<>();
+
+    public static void main (String[] args) throws Exception {
+
+        solve();
+
+        bw.write(sb.toString());
+        bw.flush();
+
+    }
+
+    static void solve () throws Exception {
+
+        A = readInt(); B = readInt();
+
+        for (int i=1; i<=10; i++) backTracking(i);
+
+        sb.append(count);
+
+    }
+
+
+    static void backTracking (int max) {
+
+        if (stack.size()==max) {
+            String s="";
+            for (int i = 0; i < max; i++) s+=Integer.toString(stack.get(i));
+            long l = Long.parseLong(s);
+            if (A<=l && l<=B) count++;
+            return;
+        }
+
+        stack.push(4);
+        backTracking(max);
+        stack.pop();
+
+        stack.push(7);
+        backTracking(max);
+        stack.pop();
+
+
+    } // 백 트래킹
+
+    static int readInt() {
+        return in.nextInt();
+    }
+
+    static long readLong() {
+        return in.nextLong();
+    }
+
+    static double readDouble() {
+        return in.nextDouble();
+    }
+
+    static String readLine() {
+        return in.nextLine();
+    }
+
+    static String read() {
+        return in.next();
+    }
+
+    static void exit (String s) {
+
+        System.out.println(s);
+        System.exit(0);
+
+    }
+
+}
+
+class FastReader {
+
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+
+}
