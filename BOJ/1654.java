fde61ab30567f4194a384a28c050809521a2da53
pache_pot <heedoo0303@naver.com>
2021-08-12 15:03:35 +0900

    # https://www.acmicpc.net/problem/1654

---
+++ BOJ/1654.java
@@ -0,0 +1,126 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int K, N;
+
+    static int[] lan;
+
+    public static void main (String[] args) throws Exception {
+
+        solve();
+
+        bw.write(sb.toString());
+        bw.flush();
+
+    }
+
+    static void solve () throws Exception {
+
+        K = readInt();
+        N = readInt();
+
+        lan = new int[K];
+        for (int i=0; i<K; i++) lan[i] = readInt();
+
+        binarySearch(1, Integer.MAX_VALUE);
+
+    }
+
+    static void binarySearch (long left, long right) {
+
+        long mid=(left+right)/2;
+
+        long sum=0;
+        for (int i=0; i<K; i++) sum += lan[i]/mid;
+
+        if (left>right) sb.append(mid);
+
+        else if (sum>=N) binarySearch(mid+1,right);
+
+        else binarySearch(left,mid-1);
+
+
+    } // 이분 탐색
+
+    static int readInt() {
+        return in.nextInt();
+    }
+
+    static long readLong() {
+        return in.nextLong();
+    }
+
+    static double readDouble() {
+        return in.nextDouble();
+    }
+
+    static String readLine() {
+        return in.nextLine();
+    }
+
+    static String read() {
+        return in.next();
+    }
+
+    static void exit (String s) {
+
+        System.out.println(s);
+        System.exit(0);
+
+    }
+
+}
+
+class FastReader {
+
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+
+}
