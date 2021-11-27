53aaf0994fa6ef91b13ef5ff063c047ec5ed33bd
pache_pot <heedoo0303@naver.com>
2021-11-27 14:40:38 +0900

    # https://www.acmicpc.net/problem/1436

---
+++ BOJ/1436.java
@@ -0,0 +1,105 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main (String[] args) throws Exception {
+
+        solve();
+
+        bw.write(sb.toString());
+        bw.flush();
+
+    }
+
+    static void solve () throws Exception {
+
+        ArrayList<Integer> sixs = new ArrayList<>();
+
+        for (int i = 666; sixs.size()<10000; i++) if (Integer.toString(i).contains("666")) sixs.add(i);
+
+        sb.append(sixs.get(readInt()-1));
+
+    }
+
+
+    static int readInt() {
+        return in.nextInt();
+    }
+
+    static long readLong() {
+        return in.nextLong();
+    }
+
+    static double readDouble() {
+        return in.nextDouble();
+    }
+
+    static String readLine() {
+        return in.nextLine();
+    }
+
+    static String read() {
+        return in.next();
+    }
+
+    static void exit (String s) {
+
+        System.out.println(s);
+        System.exit(0);
+
+    }
+
+}
+
+class FastReader {
+
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+
+}
